fc24b8ca525563451af1ec14ce97e951f2db6bb4
deepakdubal <deepak.dubalmca@gmail.com>
2015-02-28 00:29:28 +0530

    Image Compress And Resize
    Image Compress And Resize tool for window

---
+++ ImageCompressAndResize/FrmMain.Designer.cs
@@ -0,0 +1,368 @@
+﻿namespace ImageCompressAndResize
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radResizeImage = new System.Windows.Forms.RadioButton();
+            this.radKeepOriginal = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResizeWidth = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbFont = new System.Windows.Forms.ComboBox();
+            this.txtWaterMarkText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.txtXposition = new System.Windows.Forms.TextBox();
+            this.txtYposition = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbPosition = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.radResizeImage);
+            this.groupBox1.Controls.Add(this.radKeepOriginal);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtResizeWidth);
+            this.groupBox1.Location = new System.Drawing.Point(12, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(356, 106);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image Settings";
+            // 
+            // radResizeImage
+            // 
+            this.radResizeImage.AutoSize = true;
+            this.radResizeImage.Checked = true;
+            this.radResizeImage.Location = new System.Drawing.Point(103, 42);
+            this.radResizeImage.Name = "radResizeImage";
+            this.radResizeImage.Size = new System.Drawing.Size(89, 17);
+            this.radResizeImage.TabIndex = 5;
+            this.radResizeImage.TabStop = true;
+            this.radResizeImage.Text = "Resize Image";
+            this.radResizeImage.UseVisualStyleBackColor = true;
+            // 
+            // radKeepOriginal
+            // 
+            this.radKeepOriginal.AutoSize = true;
+            this.radKeepOriginal.Location = new System.Drawing.Point(103, 19);
+            this.radKeepOriginal.Name = "radKeepOriginal";
+            this.radKeepOriginal.Size = new System.Drawing.Size(141, 17);
+            this.radKeepOriginal.TabIndex = 4;
+            this.radKeepOriginal.Text = "Keep Original Resolution";
+            this.radKeepOriginal.UseVisualStyleBackColor = true;
+            this.radKeepOriginal.CheckedChanged += new System.EventHandler(this.radKeepOriginal_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "px";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(120, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Image Size";
+            // 
+            // txtResizeWidth
+            // 
+            this.txtResizeWidth.Location = new System.Drawing.Point(201, 65);
+            this.txtResizeWidth.Name = "txtResizeWidth";
+            this.txtResizeWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtResizeWidth.TabIndex = 0;
+            this.txtResizeWidth.Text = "500";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSize = true;
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.cmbPosition);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.txtYposition);
+            this.groupBox2.Controls.Add(this.txtXposition);
+            this.groupBox2.Controls.Add(this.txtSize);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cmbFont);
+            this.groupBox2.Controls.Add(this.txtWaterMarkText);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(356, 236);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Water Mark Setting";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Font";
+            // 
+            // cmbFont
+            // 
+            this.cmbFont.FormattingEnabled = true;
+            this.cmbFont.Items.AddRange(new object[] {
+            "Microsoft Sans Serif",
+            "Verdana"});
+            this.cmbFont.Location = new System.Drawing.Point(105, 66);
+            this.cmbFont.Name = "cmbFont";
+            this.cmbFont.Size = new System.Drawing.Size(121, 21);
+            this.cmbFont.TabIndex = 2;
+            // 
+            // txtWaterMarkText
+            // 
+            this.txtWaterMarkText.Location = new System.Drawing.Point(105, 35);
+            this.txtWaterMarkText.Name = "txtWaterMarkText";
+            this.txtWaterMarkText.Size = new System.Drawing.Size(205, 20);
+            this.txtWaterMarkText.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Water Mark Text";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Size";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 171);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "X Position";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Y Position";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(105, 100);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 7;
+            // 
+            // txtXposition
+            // 
+            this.txtXposition.Location = new System.Drawing.Point(105, 168);
+            this.txtXposition.Name = "txtXposition";
+            this.txtXposition.Size = new System.Drawing.Size(100, 20);
+            this.txtXposition.TabIndex = 8;
+            // 
+            // txtYposition
+            // 
+            this.txtYposition.Location = new System.Drawing.Point(105, 197);
+            this.txtYposition.Name = "txtYposition";
+            this.txtYposition.Size = new System.Drawing.Size(100, 20);
+            this.txtYposition.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 11;
+            // 
+            // cmbPosition
+            // 
+            this.cmbPosition.FormattingEnabled = true;
+            this.cmbPosition.Items.AddRange(new object[] {
+            "Center",
+            "Top",
+            "Bottom"});
+            this.cmbPosition.Location = new System.Drawing.Point(105, 136);
+            this.cmbPosition.Name = "cmbPosition";
+            this.cmbPosition.Size = new System.Drawing.Size(121, 21);
+            this.cmbPosition.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 139);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(44, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Position";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtPath);
+            this.groupBox3.Controls.Add(this.label10);
+            this.groupBox3.Location = new System.Drawing.Point(12, 377);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(356, 51);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Save to";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(13, 25);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(105, 18);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(196, 20);
+            this.txtPath.TabIndex = 10;
+            this.txtPath.Text = "C:\\ResizeImages";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.ForeColor = System.Drawing.Color.LimeGreen;
+            this.lblMsg.Location = new System.Drawing.Point(164, 443);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(33, 13);
+            this.lblMsg.TabIndex = 6;
+            this.lblMsg.Text = "Done";
+            this.lblMsg.Visible = false;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 495);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmMain";
+            this.Text = "Image Resize And Compress";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResizeWidth;
+        private System.Windows.Forms.RadioButton radResizeImage;
+        private System.Windows.Forms.RadioButton radKeepOriginal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtWaterMarkText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbFont;
+        private System.Windows.Forms.TextBox txtYposition;
+        private System.Windows.Forms.TextBox txtXposition;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbPosition;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ ImageCompressAndResize/FrmMain.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageCompressAndResize
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitializeOpenFileDialog();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblMsg.Visible = false;
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+             if (dr == System.Windows.Forms.DialogResult.OK)
+             {
+                 bool keepOriginal = radKeepOriginal.Checked;
+                 string OutputFolder = txtPath.Text.Trim();
+                 if (!Directory.Exists(OutputFolder)) Directory.CreateDirectory(OutputFolder);
+                 int ResizeWidth = string.IsNullOrEmpty(txtResizeWidth.Text) ? 500 : Convert.ToInt32(txtResizeWidth.Text.Trim());
+
+                 string waterMarkText = string.IsNullOrEmpty(txtWaterMarkText.Text) ? "" : txtWaterMarkText.Text.Trim();
+                 int xp =   string.IsNullOrEmpty(txtXposition.Text) ? 77 : Convert.ToInt32(txtXposition.Text.Trim());
+                 int yp =   string.IsNullOrEmpty(txtYposition.Text) ? 77 : Convert.ToInt32(txtYposition.Text.Trim());
+                 string WaterMarkPos = cmbPosition.SelectedItem.ToString();
+                 string wfont = cmbFont.SelectedText ;
+                 int wsize = string.IsNullOrEmpty(txtSize.Text) ? 16 : Convert.ToInt32(txtSize.Text.Trim());
+
+                 foreach (String file in this.openFileDialog1.FileNames)
+                 {
+                     string strfilename = file.Substring(file.LastIndexOf("\\") + 1, file.Length - file.LastIndexOf("\\") - 1);
+
+                     CreateThumb(file, OutputFolder + "\\" + strfilename, waterMarkText , ResizeWidth, keepOriginal,WaterMarkPos,wfont,wsize);
+                 }
+
+             }
+             lblMsg.Visible = true;
+
+        }
+
+        private void InitializeOpenFileDialog()
+        {
+            // Set the file dialog to filter for graphics files.
+            //  this.openFileDialog1.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" +  "All files (*.*)|*.*";
+            this.openFileDialog1.Filter = "Images (*.JPG)|*.JPG";
+            //  Allow the user to select multiple images.
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "My Image Browser";
+        }
+
+        public void CreateThumb(string FileReadFrom, string FileSaveTo, string WaterMarkText, int ThumbWidth, bool keepOriginal, string waterMarkpos,string wfont,int wsize)
+        {
+
+            string imageURL;
+            int imageHeight = 0;
+            int imageWidth = 0;
+            int CurrentimgHeight = 0;
+            int CurrentimgWidth = 0;
+            // string savepath = Server.MapPath(FileSaveTo);
+            string savepath = FileSaveTo;
+            // imageURL = Server.MapPath("~/Photos/HDPhotos/" + FileReadFrom);
+            imageURL = FileReadFrom;
+
+            System.Drawing.Image fullSizeImg;
+            fullSizeImg = System.Drawing.Image.FromFile(imageURL);
+            CurrentimgHeight = fullSizeImg.Height;
+            CurrentimgWidth = fullSizeImg.Width;
+            if (!keepOriginal)
+            {
+                imageHeight = (CurrentimgHeight * ThumbWidth) / CurrentimgWidth;
+                imageWidth = (CurrentimgWidth * ThumbWidth) / CurrentimgHeight;
+
+                if (imageHeight > ThumbWidth)
+                {
+                    imageHeight = ThumbWidth;
+                }
+                else
+                {
+                    imageWidth = ThumbWidth;
+                }
+            }
+            else
+            {
+                imageHeight = CurrentimgHeight;
+                imageWidth = CurrentimgWidth;
+            }
+
+            //This will only work for jpeg images
+            // Response.ContentType = "image/jpeg";
+            if ((imageHeight > 0) && (imageWidth > 0))
+            {
+                System.Drawing.Image.GetThumbnailImageAbort dummyCallBack;
+                dummyCallBack = new System.Drawing.Image.GetThumbnailImageAbort(ThumbnailCallback);
+                System.Drawing.Image thumbNailImg;
+                //thumbNailImg = new fullSizeImg.GetThumbnailImage(imageWidth, imageHeight, dummyCallBack, IntPtr.Zero);
+                thumbNailImg = fullSizeImg.GetThumbnailImage(imageWidth, imageHeight, dummyCallBack, IntPtr.Zero);
+                SizeF StringSizeF;
+                Single DesiredWidth;
+                Font wmFont;
+                Single RequiredFontSize;
+                Single Ratio;
+
+                System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(thumbNailImg);
+                string strWatermark = WaterMarkText;
+
+                if (WaterMarkText.Trim() == "")
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(fullSizeImg, 0, 0, imageWidth, imageHeight);
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    thumbNailImg.Save(savepath, ImageFormat.Jpeg);
+                    // SaveImage(Session["U"].ToString(), FileReadFrom);
+                }
+                else
+                { //Set the watermark font	
+                    wmFont = new Font(wfont, wsize, FontStyle.Bold);
+
+                    // DesiredWidth = imageWidth * Convert.ToInt32(0.5);
+                    DesiredWidth = imageWidth / 2;
+                    //use the MeasureString method to position the watermark in the centre of the image
+
+                    StringSizeF = g.MeasureString(strWatermark, wmFont);
+                    Ratio = StringSizeF.Width / wmFont.SizeInPoints;
+                    RequiredFontSize = DesiredWidth / Ratio;
+
+
+                    wmFont = new Font(wfont, RequiredFontSize, FontStyle.Bold);
+                    // 'Sets the interpolation mode for a high quality image  
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(fullSizeImg, 0, 0, imageWidth, imageHeight);
+                    g.SmoothingMode = SmoothingMode.HighQuality;
+                    SolidBrush letterBrush = new SolidBrush(Color.FromArgb(50, 255, 255, 255));
+                    SolidBrush shadowBrush = new SolidBrush(Color.FromArgb(50, 0, 0, 0));
+                    // 'Enter the watermark text 
+                    if (waterMarkpos == "Center")
+                    {
+                        g.DrawString(WaterMarkText, wmFont, shadowBrush, 75, ((imageHeight / 2) - 36));
+                        g.DrawString(WaterMarkText, wmFont, letterBrush, 77, ((imageHeight / 2) - 38));
+                    }
+
+                    if (waterMarkpos == "Top")
+                    {
+                        g.DrawString(WaterMarkText, wmFont, shadowBrush, 10, 10);
+                        g.DrawString(WaterMarkText, wmFont, letterBrush, 12, 10);
+                    }
+
+                    if (waterMarkpos == "Bottom")
+                    {
+                        g.DrawString(WaterMarkText, wmFont, shadowBrush, 10, (imageHeight - 36));
+                        g.DrawString(WaterMarkText, wmFont, letterBrush, 12, (imageHeight - 38));
+                    }
+                    // thumbNailImg.Save(Response.OutputStream, ImageFormat.Jpeg);
+                    thumbNailImg.Save(savepath, ImageFormat.Jpeg);
+                }
+
+                thumbNailImg.Dispose();
+                g.Dispose();
+
+            }
+            else
+            {
+                // fullSizeImg.Save(Response.OutputStream, ImageFormat.Jpeg);
+                //       '  fullSizeImg.Save("C:\test_water.jpg", ImageFormat.Jpeg)
+
+
+            }
+
+
+            //'Important, dispose of the image  – otherwise the image file will be locked by the server for several minutes
+
+            fullSizeImg.Dispose();
+
+
+        }
+
+        public Boolean ThumbnailCallback()
+        {
+            return false;
+        }
+
+        private void radKeepOriginal_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
